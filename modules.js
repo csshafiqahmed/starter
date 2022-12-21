e2d4e9d96d7930484888a69b04f370e988f6a719
csshafiqahmed <csshafiqahmed@gmail.com>
2022-12-21 15:59:16 +0500

    Learning about exports and module.exports

--- modules.js
+++ modules.js
@@ -1,2 +1,11 @@
-console.log(arguments);
-console.log(require('module').wrapper);
+//console.log(arguments);
+//console.log(require('module').wrapper);
+//module.exports
+const calculator = require('./test-module-1');
+
+const calc1 = new calculator();
+console.log(calc1.add(2, 5));
+
+//exports
+const calc2 = require('./test-module-2');
+console.log(calc2.add(2, 5));
